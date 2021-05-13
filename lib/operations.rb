93a06d13053faf28a9e9163e197691d732b9102d
Shasha Yan <shashayan@gmail.com>
2021-05-13 03:31:17 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,10 @@
 def unsafe?(speed)
   if speed > 60
-    true
+    answer == true
   if speed < 40
-    true
+    answer == true
   if 40 < speed < 60
-    false
+    answer == false
   end
 end
 
